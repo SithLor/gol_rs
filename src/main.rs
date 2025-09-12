577037e3b552985263554ac1355dd6277b96de8f
Gavin Rhoades <84148006+SithLor@users.noreply.github.com>
2025-09-12 13:20:51 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-const CONFIG_FPS: u32 = 60;// 24, 30, 60 
+const CONFIG_FPS: u32 = 30;// 24, 30, 60 
 const CONFIG_VIDEO_LENGTH_SECONDS: u32 = 10;// 10, 30, 60
-const CONFIG_X: u32 = 500; // 1920, 2560, 3840 ,
-const CONFIG_Y: u32 = 500; // 1080, 1440, 2160 
+const CONFIG_X: u32 = 512; // 1920, 2560, 3840 ,
+const CONFIG_Y: u32 = 512; // 1080, 1440, 2160 
 const CONFIG_SAVE_LOCATION: &str = "./src/frames";
 const CONFIG_MAX_ITERATIONS: u32 = CONFIG_FPS * CONFIG_VIDEO_LENGTH_SECONDS;
 
