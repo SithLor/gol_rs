d28bf1e2e74a3d307b3cd716e87654e71e31dc28
Gavin Rhoades <84148006+SithLor@users.noreply.github.com>
2025-11-03 08:24:48 -0800

    Enable AVX512 step kernel when AVX2 is detected
    intel support for avx512 suck asss

--- src/main.rs
+++ src/main.rs
@@ -294,9 +294,9 @@ mod faster_hw {
                 *mode.write().unwrap() = "fast_hw_DISABLE_AVX256";
                 //println!("AVX2 detected");
                 unsafe {
-                    step_kernel_scalar(current, rows, cols, out);
+                    //step_kernel_scalar(current, rows, cols, out);
         //            step_kernel_avx2(current, rows, cols, out);
-                   // step_kernel_avx512(current, rows, cols, out);
+                   step_kernel_avx512(current, rows, cols, out);
 
                 }
                 return;
